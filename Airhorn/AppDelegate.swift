140afab8eb7125d7d023749d56b5025c24f1ac67
Brad Bohen <bbohen@gmail.com>
2019-08-10 21:13:52 -0400

    Initial Commit

---
+++ Airhorn/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Airhorn
+//
+//  Created by Brad Bohen on 8/10/19.
+//  Copyright © 2019 Brad Bohen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Airhorn/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Airhorn
+//
+//  Created by Brad Bohen on 8/10/19.
+//  Copyright © 2019 Brad Bohen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
